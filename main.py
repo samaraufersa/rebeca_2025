efb295888e0bd61757a12c3c680f861034221d15
samaraufersa <109767273+samaraufersa@users.noreply.github.com>
2025-09-10 13:41:09 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+df = pd.read_csv('WHO_time_series.csv')
+
+df['Date_reported'] = pd.to_datetime(df['Date_reported'])
+
+fig1 = px.line(df,
+        x = 'Date_reported',
+        y = 'Cumulative_cases',
+        color = 'Country',
+        title = 'Número de Casos Acumulados por COVID')
+fig1.update_layout(xaxis_title = 'Data', yaxis_title = 'Número de Casos Acumulados')
+fig1.show()
+
+st.plotly_chart(fig1, use_container_width=True)
